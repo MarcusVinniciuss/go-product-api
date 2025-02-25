e24e4f50c802a94546c5d22650c5e1c05c728c47
MarcusVinniciuss <marcus.vinnicius02@gmail.com>
2025-02-25 01:29:34 -0300

    create project structure

---
+++ cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"products-api/controller"
+	"products-api/usecase"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main()  {
+	
+	server := gin.Default()
+
+	ProductUseCase := usecase.NewProductUseCase()
+	//Controller layers
+	ProductController := controller.NewProductController(ProductUseCase)
+
+	server.GET("/ping", func(ctx *gin.Context){
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	server.GET("/products", ProductController.GetProducts)
+
+	server.Run(":8000")
+}
---
+++ controller/product_controller.go
@@ -0,0 +1,32 @@
+package controller
+
+import (
+	"net/http"
+	"products-api/model"
+	"products-api/usecase"
+
+	"github.com/gin-gonic/gin"
+)
+
+type productController struct {
+	productUseCase usecase.ProductUsecase
+}
+
+func NewProductController(usecase usecase.ProductUsecase) productController {
+	return productController{
+		productUseCase: usecase,
+	}
+}
+
+func (p *productController) GetProducts(ctx *gin.Context) {
+	
+	products := []model.Product{
+		{
+			ID: 1,
+			Name: "Monitor",
+			Price: 20,
+		},
+	}
+
+	ctx.JSON(http.StatusOK, products)
+}
