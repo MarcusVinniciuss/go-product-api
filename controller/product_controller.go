a8ad61cec76625ab8e080369ccba5115a914cf60
MarcusVinniciuss <marcus.vinnicius02@hotmail.com>
2025-02-26 02:08:38 -0300

    get product by id

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -2,7 +2,9 @@ package controller
 
 import (
 	"net/http"
+	"products-api/model"
 	"products-api/usecase"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -25,4 +27,59 @@ func (p *productController) GetProducts(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, products)
+}
+
+func (p *productController) CreateProduct(ctx *gin.Context) {
+
+	var product model.Product
+	err := ctx.BindJSON(&product)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	insertedProduct, err := p.productUseCase.CreateProduct(product)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, insertedProduct)
+}
+
+func (p *productController) GetProductsById(ctx *gin.Context) {
+	
+	id := ctx.Param("productId")
+	if id == ""{
+		response := model.Response{
+			Message: "Id do produto não pode ser nulo",
+		}
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	productId, err := strconv.Atoi(id)
+	if err != nil{
+		response := model.Response{
+			Message: "Id do produto precisa ser um número",
+		}
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	product, err := p.productUseCase.GetProductsById(productId)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
+	}
+
+	if product == nil{
+		response := model.Response{
+			Message: "Produto nao foi encontrado",
+		}
+		ctx.JSON(http.StatusNotFound, response)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, product)
 }
